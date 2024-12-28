65739e75ed432dec063a98553d9e5b5fc53714f9
ErickStudios <152753651+ErickStudios@users.noreply.github.com>
2024-12-28 13:49:35 -0600

    vfggf

--- Markarian VirtualUEFI/Markarian/VirtualBoot/MainWindow.xaml.cs
+++ Markarian VirtualUEFI/Markarian/VirtualBoot/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 using System.Windows.Threading;
 
 namespace Markarian_VirtualUEFI
@@ -14,6 +16,66 @@ namespace Markarian_VirtualUEFI
         {
             InitializeComponent();
             InitializeProgressBar();
+            CreateChildWindow("A");
+        }
+
+        private void CreateChildWindow(string title)
+        {
+            // Crear la ventana base
+            var window = new Grid
+            {
+                Name = title,
+                Width = 300,
+                Height = 200,
+                Background = new SolidColorBrush(Color.FromRgb(255, 255, 255))
+            };
+
+            // Crear la barra de título de la ventana
+            var windowTitleBar = new Grid
+            {
+                Name = "TitleBar",
+                Height = 30,
+                Background = new SolidColorBrush(Color.FromRgb(75, 225, 255)),
+                VerticalAlignment = VerticalAlignment.Top
+            };
+
+            // Crear el texto de la barra de título
+            var windowTitleBarText = new TextBlock
+            {
+                Name = "Title",
+                Text = title,
+                VerticalAlignment = VerticalAlignment.Center,
+                Margin = new Thickness(10, 0, 0, 0)
+            };
+
+            // Crear el botón de cerrar
+            var windowCloseButton = new Button
+            {
+                Name = "close",
+                Content = "x",
+                Width = 30,
+                Height = 30,
+                Background = new SolidColorBrush(Color.FromRgb(246, 211, 217)),
+                HorizontalAlignment = HorizontalAlignment.Right,
+                VerticalAlignment = VerticalAlignment.Center
+            };
+
+            windowCloseButton.Click += (s, e) => CloseChildWindow(window);
+
+            // Añadir los elementos a la barra de título
+            windowTitleBar.Children.Add(windowTitleBarText);
+            windowTitleBar.Children.Add(windowCloseButton);
+
+            // Añadir la barra de título a la ventana
+            window.Children.Add(windowTitleBar);
+
+            // Añadir la ventana al contenedor principal
+            MainGrid.Children.Add(window);
+        }
+
+        private void CloseChildWindow(Grid window)
+        {
+            MainGrid.Children.Remove(window);
         }
 
         private void InitializeProgressBar()
